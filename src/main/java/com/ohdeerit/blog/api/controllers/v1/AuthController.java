1d533f4d8aaf7e04d75dbaa1af8f41940a76bc89
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2025-06-15 19:50:33 +0200

    feat(auth): adjust login reponse, add logout endpoint

--- src/main/java/com/ohdeerit/blog/api/controllers/v1/AuthController.java
+++ src/main/java/com/ohdeerit/blog/api/controllers/v1/AuthController.java
@@ -1,18 +1,20 @@
 package com.ohdeerit.blog.api.controllers.v1;
 
+import com.ohdeerit.blog.services.interfaces.AuthenticationService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
-import com.ohdeerit.blog.api.request.LoginRequest;
 import com.ohdeerit.blog.api.response.AuthResponse;
-import com.ohdeerit.blog.services.interfaces.AuthenticationService;
+import com.ohdeerit.blog.api.request.LoginRequest;
 import com.ohdeerit.blog.config.SecurityConstants;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpHeaders;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping(path = "/api/v1/auth")
@@ -21,7 +23,7 @@ public class AuthController {
 
     private final AuthenticationService authenticationService;
 
-    @PostMapping
+    @PostMapping(path = "/login" )
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
         final UserDetails userDetails = authenticationService.authenticate(
                 loginRequest.email(),
@@ -37,10 +39,26 @@ public class AuthController {
                 .sameSite("Strict")
                 .build();
 
-        final AuthResponse authResponse = new AuthResponse(SecurityConstants.SESSION_DURATION_SECONDS);
+        final LocalDateTime expiresAt = LocalDateTime.now().plusSeconds(SecurityConstants.SESSION_DURATION_SECONDS);
+        final AuthResponse authResponse = new AuthResponse(expiresAt);
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
                 .body(authResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<AuthResponse> logout() {
+        ResponseCookie jwtCookie = ResponseCookie.from(SecurityConstants.JWT_COOKIE_NAME, "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(new AuthResponse(null));
     }
 }
--- src/main/java/com/ohdeerit/blog/api/response/AuthResponse.java
+++ src/main/java/com/ohdeerit/blog/api/response/AuthResponse.java
@@ -1,6 +1,8 @@
 package com.ohdeerit.blog.api.response;
 
+import java.time.LocalDateTime;
+
 public record AuthResponse(
-        long expiresIn
+        LocalDateTime expiresAt
 ) {
 }
